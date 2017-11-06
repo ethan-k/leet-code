ddf38dc457a4f50a40e6685c7a1c12d3f35df77e
eunseok.kang <eunseok.kang@rakuten.com>
2017-11-06 18:00:15 +0900

    Change: Init

---
+++ src/leecode/FizzBuzz.java
@@ -0,0 +1,15 @@
+package leecode;
+
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+
+    }
+
+	public static List<String> fizzBuzz(int n) {
+
+	}
+}

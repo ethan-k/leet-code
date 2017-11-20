1ad307c0fee7aaea41382f50436e82078ad5d3be
eunseok.kang <ethan.es.kang@gmail.com>
2017-11-20 20:25:31 +0900

    Longgest Uncommon Subsequence

--- src/leecode/LongestUncommonSubsequence.java
+++ src/leecode/LongestUncommonSubsequence.java
@@ -1,18 +1,30 @@
 package leecode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class LongestUncommonSubsequence {
 
-//    public static void main(String[] args) {
-//
-//		LongestUncommonSubsequence longestUncommonSubsequence = new LongestUncommonSubsequence();
-//		System.out.println(longestUncommonSubsequence.findLUSlength("aba", "cdc"));
-//    }
-//
-//	public int findLUSlength(String a, String b) {
-//
-//	}
+	public static void main(String[] args) {
+
+		LongestUncommonSubsequence longestUncommonSubsequence = new LongestUncommonSubsequence();
+		System.out.println(longestUncommonSubsequence.findLUSlength("ada", "aba"));
+	}
+
+	public int findLUSlength(String a, String b) {
+		if (a.equals(b)) {
+			return -1;
+		}
+		if (a.length() > b.length()) {
+			return a.length();
+		} else if (a.length() < b.length()) {
+			return b.length();
+		} else {
+			int max = a.length();
+			for (int i = 0; i < b.length(); i++) {
+				if (a.indexOf(b.substring(i)) == -1) {
+					max = b.length() - i;
+					break;
+				}
+			}
+			return max;
+		}
+	}
 }

0914158c0a740baef1d76ac2b8fc9c6201142c77
Eunseok Kang <kes0228@gmail.com>
2019-08-01 20:28:19 +0800

    - Solved Range Sum of BST

---
+++ src/main/java/leecode/RangeSumOfBST.java
@@ -0,0 +1,23 @@
+package leecode;
+
+import leecode.model.TreeNode;
+
+public class RangeSumOfBST {
+  private int sum = 0;
+
+  public int rangeSumBST(TreeNode root, int L, int R) {
+    rangeSumBSTHelper(root, L, R);
+    return sum;
+  }
+
+  public void rangeSumBSTHelper(TreeNode root, int L, int R) {
+    if (root == null) {
+      return;
+    }
+    rangeSumBSTHelper(root.left, L, R);
+    if (root.val >= L && root.val <= R) {
+      sum += root.val;
+    }
+    rangeSumBSTHelper(root.right, L, R);
+  }
+}

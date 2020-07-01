a6d5e900f58e10c507894cbd7bdbb6e584d73d6a
Eunseok Kang <kes0228@gmail.com>
2020-07-01 08:27:22 +0800

    - Solved Decompress Run-Length Encoded List

--- README.md
+++ README.md
@@ -175,6 +175,7 @@
 158. [Minimum Time Visiting All Points](https://leetcode.com/problems/minimum-time-visiting-all-points/)
 159. [How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/)
 160. [Find Numbers with Even Number of Digits](https://leetcode.com/problems/find-numbers-with-even-number-of-digits/)
+161. [Decompress Run-Length Encoded List](https://leetcode.com/problems/decompress-run-length-encoded-list/)
 
 ## To Solve
 
---
+++ src/main/java/leecode/array/DecompressRunLengthEncodedList.java
@@ -0,0 +1,24 @@
+package leecode.array;
+
+public class DecompressRunLengthEncodedList {
+  public int[] decompressRLElist(int[] nums) {
+    int totalFreq = 0;
+    for (int i = 0; i < nums.length; i = i + 2) {
+      totalFreq += nums[i];
+    }
+
+    int[] ret = new int[totalFreq];
+    int startIndex = 0;
+    for (int i = 0; i < nums.length; i = i + 2) {
+      int freq = nums[i];
+      int val = nums[i + 1];
+      int x = 0;
+      while (x < freq) {
+        ret[startIndex++] = val;
+        x++;
+      }
+    }
+
+    return ret;
+  }
+}

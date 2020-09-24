f1e234c082f9534e33766f1195db94f5357b261a
Eunseok Kang <kes0228@gmail.com>
2020-09-24 12:22:01 +0800

    - Updated Target Sum

--- src/main/java/leetcode/dynamic/TargetSum.java
+++ src/main/java/leetcode/dynamic/TargetSum.java
@@ -1,25 +1,32 @@
 package leetcode.dynamic;
 
 public class TargetSum {
-  int total = 0;
-  int target;
+  int S;
 
   public int findTargetSumWays(int[] nums, int S) {
-    this.target = S;
-    sum(0, 0, nums);
-    return total;
+
+    Integer[][] memo = new Integer[nums.length][2001];
+    this.S = S;
+
+    return findTargetSumWays(nums, 0, 0, memo);
   }
 
-  void sum(int curSum, int index, int[] nums) {
 
+  public int findTargetSumWays(int[] nums, int i, int sum, Integer[][] memo) {
 
-    if (index == nums.length && curSum == target) {
-      total++;
-      return;
-    } else if (index == nums.length) {
-      return;
+    if (i == nums.length && sum == S)
+      return 1;
+    else if (i == nums.length)
+      return 0;
+
+    if (memo[i][sum+1000] != null) {
+      return memo[i][sum+1000];
     }
-    sum(curSum + nums[index], index + 1, nums);
-    sum(curSum - nums[index], index + 1, nums);
+
+    int plus  = findTargetSumWays(nums, i+1, sum - nums[i], memo);
+    int minus = findTargetSumWays(nums, i+1, sum + nums[i], memo);
+
+    memo[i][sum+1000] = plus + minus;
+    return memo[i][sum+1000];
   }
 }

4d2c2cb669ee3f16ae6c4377b5a677491d8d5d14
Eunseok Kang <kes0228@gmail.com>
2020-10-04 22:03:35 +0800

    - Updated Letter Case Permutation with more efficient algorithm

--- src/main/java/leetcode/LetterCasePermutation.java
+++ src/main/java/leetcode/LetterCasePermutation.java
@@ -1,9 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class LetterCasePermutation {
 
@@ -16,30 +14,26 @@ public class LetterCasePermutation {
     System.out.println(new LetterCasePermutation().letterCasePermutation("FKqeaCFIESzo"));
   }
 
-  public List<String> letterCasePermutation(String S) {
-    Set<String> result = new HashSet<>();
-    bfs("", S, result);
+  List<String> ret = new ArrayList<>();
 
-    return new ArrayList<>(result);
+  public List<String> letterCasePermutation(String S) {
+    letterCasePermutation(S.toCharArray(), 0);
+    return ret;
   }
 
-  public void bfs(String prefix, String s, Set<String> result) {
-    if (s.length() == 0) {
-      result.add(prefix);
-
+  void letterCasePermutation(char[] chars, int idx) {
+    if (idx == chars.length) {
+      ret.add(new String(chars));
       return;
     }
-    StringBuilder builder = new StringBuilder();
-    builder.append(prefix);
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      bfs(builder.toString() + c, s.substring(i + 1), result);
-      if (Character.isLowerCase(c)) {
-        bfs(builder.toString() + Character.toUpperCase(c), s.substring(i + 1), result);
-      } else if (Character.isUpperCase(c)) {
-        bfs(builder.toString() + Character.toLowerCase(c), s.substring(i + 1), result);
-      }
-      builder.append(c);
+
+    if (Character.isAlphabetic(chars[idx])) {
+      chars[idx] = Character.toLowerCase(chars[idx]);
+      letterCasePermutation(chars.clone(), idx + 1);
+      chars[idx] = Character.toUpperCase(chars[idx]);
+      letterCasePermutation(chars.clone(), idx + 1);
+    } else {
+      letterCasePermutation(chars, idx + 1);
     }
   }
 

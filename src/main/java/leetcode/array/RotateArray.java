fd05b3f9f35e51983c8e5c8290efe1f880892afc
Eunseok Kang <kes0228@gmail.com>
2020-10-16 09:03:28 +0800

    - Solved Rotate Array with constant space

--- src/main/java/leetcode/array/RotateArray.java
+++ src/main/java/leetcode/array/RotateArray.java
@@ -14,4 +14,27 @@ public class RotateArray {
       nums[i] = tmp[i];
     }
   }
+
+  public void rotateWithConstantSpace(int[] nums, int k) {
+
+    k = k % nums.length;
+    if (k == 0) {
+      return;
+    }
+
+    reverse(nums, 0, nums.length-1);
+    reverse(nums, 0, k-1);
+    reverse(nums, k, nums.length-1);
+
+  }
+
+  public void reverse(int[] nums, int start, int end) {
+    while(start<=end) {
+      int tmp = nums[end];
+      nums[end] = nums[start];
+      nums[start] = tmp;
+      start++;
+      end--;
+    }
+  }
 }
